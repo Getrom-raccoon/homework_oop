fa99f2521d2c92322ea2b2ab3491e3c618d55628
getrom-raccoon <getromgleb@gmail.com>
2025-10-31 20:13:33 +0300

    new brain

--- src/category.py
+++ src/category.py
@@ -1,20 +1,40 @@
 from typing import List
-
 from src.product import Product
 
 
 class Category:
-    """
-    Класс для категорий товаров.
-    """
 
     category_count = 0
-    product_count = 0
+    product_total = 0
 
-    def __init__(self, name: str, description: str, products: List[Product]):
+    def __init__(self, name: str, description: str, products: List[Product] = None):
+        """
+        Инициализация Category.
+
+        Args:
+            name: Название категории.
+            description: Описание категории.
+            products: Список товаров (по умолчанию пустой).
+        """
         self.name = name
         self.description = description
         self.products = products if products else []
-
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_total += len(self.products)
+
+    def add_product(self, product: Product):
+        """
+        Добавляет товар в категорию.
+
+        Args:
+            product: Экземпляр товара.
+        """
+        self.products.append(product)
+        Category.product_total += 1
+
+    @property
+    def product_count(self):
+        """
+        Возвращает количество товаров в категории.
+        """
+        return len(self.products)
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,17 +1,17 @@
 from src.product import Product
 
-
 def test_product_init():
-    product = Product("Телевизор", "40-дюймовый телевизор", 30000.5, 15)
-    assert product.name == "Телевизор"
-    assert product.description == "40-дюймовый телевизор"
+    product = Product("Тестовый товар", "Описание", 30000.5, 15)
+    assert product.name
+    assert product.description == "Описание"
     assert product.price == 30000.5
     assert product.quantity == 15
 
-
 def test_product_price_and_quantity():
-    product = Product("Ноутбук", "Мощный", 50000, 2)
+    product = Product("Товар", "Описание", 50000, 2)
     product.price = 60000
     assert product.price == 60000
     product.quantity = 3
     assert product.quantity == 3
+    product.price = -100
+    assert product.price == 0
